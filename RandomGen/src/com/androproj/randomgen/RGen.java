4a2c830c358da481670952fc686484e9dc6440d6
Rajeef <rajeefmk@gmail.com>
2014-05-20 15:57:47 +0530

    First commit with all projects

---
+++ RandomGen/src/com/androproj/randomgen/RGen.java
@@ -0,0 +1,48 @@
+package com.androproj.randomgen;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class RGen extends ActionBarActivity {
+
+	TextView mResult;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_rgen);
+
+		TextView mTextView1;
+		TextView mTextView2;
+		
+		
+		
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.rgen, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	
+
+}
