26573c0843acf3c2bc2e29b09deea864c5e61524
Rajeef <rajeefmk@gmail.com>
2014-06-23 13:09:55 +0530

    Commments to codes

--- ListViewTest/src/com/example/listviewtest/MainActivity.java
+++ ListViewTest/src/com/example/listviewtest/MainActivity.java
@@ -10,16 +10,30 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		//ListView layout (of the whole screen) is initialized in activity_main_simple_list.xml
 		setContentView(R.layout.activity_main_simple_list);
 
+		//String contains data required by the Adapter.
 		String[] codelearnChapters = new String[] {
 				
 				"chapter1", "chapter2","chapter3", "chapter4"
 		};
 		
+		// Adapter uses "this" context, the layout (of individual row) used to
+		//bind data from above is taken from default android layout,
+		//the third parameter in the ArrayAdapter is the "source of data" which
+		// is the codelearnChapters String array
+		
 		ArrayAdapter<String> codelearnAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, codelearnChapters);
 		
+		// ListView just like TextView and Edit Text , we inflate the layout 
+		// listId is the id given to ListView in activity_main_simple_list.xml
+		
 		ListView codeLearnLessons = (ListView) findViewById(R.id.listId);
+		
+		// setAdapter sets the adapter(codelearnAdapter) to the
+		//listView codelearnlessons
 		
 		codeLearnLessons.setAdapter(codelearnAdapter);
 	}
